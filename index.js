6bcee50a8f747ca9f72ddae20ed8488d98e884a1
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-12-01 22:16:15 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const finish = (pokemon) => {
+    const image = document.getElementById("Foto");
+    image.src = pokemon.sprites.other["official-artwork"].front_default;
+
+    const name = document.getElementById("Nome");
+    name.innerHTML = pokemon.name;
+}
+
+const urlPokemon = () => {
+    const number = Math.round(Math.random() * 150) + 1;
+    const url = `https://pokeapi.co/api/v2/pokemon/${number}`; 
+    return url;
+}
+
+const iniciar = async () => {
+    const url2 = urlPokemon();
+    const response = await fetch(url2);
+    const result = await response.json();
+    finish(result);
+};
+
+document.addEventListener('DOMContentLoaded', iniciar);
